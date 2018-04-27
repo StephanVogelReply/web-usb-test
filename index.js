1dc55b7a2e1670efa1a75131686ebd9a229e74ac
StephanVogelReply <38779097+StephanVogelReply@users.noreply.github.com>
2018-04-27 10:46:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/scripts/myscript.js', function (req, res) {
-  res.sendFile(path.join(__dirname + 'scripts/myscript.js'));
+  res.sendFile(path.join(__dirname + 'myscript.js'));
 });
 
 // add other routes below
