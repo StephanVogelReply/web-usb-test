f080a55cb52911d04e547e2dd02497ecb1fa2a75
StephanVogelReply <38779097+StephanVogelReply@users.noreply.github.com>
2018-04-30 13:26:10 +0200

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -3,40 +3,15 @@ document.addEventListener('DOMContentLoaded', event => {
 
   button.addEventListener('click', async() => {
     let device
-    const VENDOR_ID = 1234
-    	
-    try {
-	navigator.usb.getDevices().then(function(devices){
-		console.log(devices);
-		device = devices[0];
-		console.log(device);
-		//device.open()
-		//console.log('opened:', device)
-		device2 = navigator.usb.requestDevice({
-			filters: [{
-				//vendorId: VENDOR_ID
-			}]
-		})
-		console.log(device2);
-	});
-	
-	
-    } catch (error) {
-	console.log(error)    
-    }
-    /*
+    const VENDOR_ID = 0x1A61
+    
     try {
       device = await navigator.usb.requestDevice({
         filters: [{
-          //vendorId: VENDOR_ID
+          vendorId: VENDOR_ID
         }]
       })
-		
-	
-	    
-	//next
-	    
-      console.log(device)
+
       console.log('open')
       await device.open()
       console.log('opened:', device)
@@ -44,6 +19,5 @@ document.addEventListener('DOMContentLoaded', event => {
       console.log(error)
     }
 	  await device.close()
-	*/
   })
 })
